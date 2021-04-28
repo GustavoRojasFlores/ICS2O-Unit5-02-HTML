1ce6a8ff37739d34a81ffdfcccb4fee75f536040
Gustavo Rojas <>
2021-04-28 14:41:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", displayDate) // Assigns an event listener with a function to the button so it can run said function when pressed.
+
+function displayDate() {
+  alert("Sample Alert Text") // Displays an alert message saying "Sample Alert Text"
+} // Creates a new function matching the one referenced in the addEventListener function.
